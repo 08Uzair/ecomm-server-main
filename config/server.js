d16e956d5902479f4e26d72a5e8aefcc565293e9
08Uzer <121750611+08Uzair@users.noreply.github.com>
2025-06-20 12:19:40 +0530

    updated config/server.js

--- config/server.js
+++ config/server.js
@@ -1,9 +1,20 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+
+  // 👇 Add this to set the public URL of your hosted Strapi app
+  url: env('RENDER_EXTERNAL_URL', 'https://ecomm-server-main.onrender.com'),
+
   app: {
     keys: env.array('APP_KEYS'),
   },
+
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
+    },
+  },
+
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
